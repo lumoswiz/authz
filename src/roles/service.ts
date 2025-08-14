222593bdb98d002badab6f8132f483c6ee864cfb
lumoswiz <lumoswiz@protonmail.com>
2025-08-14 14:46:12 +0200

    refactor(roles): use args types

--- src/roles/service.ts
+++ src/roles/service.ts
@@ -1,52 +1,33 @@
 import type { Effect } from "effect"
 import { Context } from "effect"
-import type { Address, Hex } from "viem"
-import type { ConditionFlat, ExecutionOptions, TransactionData } from "../shared/types.js"
+import type { Address } from "viem"
+import type { TransactionData } from "../shared/types.js"
 import type { RoleError } from "./errors.js"
+import type {
+  BuildAssignRolesTxArgs,
+  BuildDeployModuleTxArgs,
+  BuildScopeFunctionTxArgs,
+  BuildScopeTargetTxArgs,
+  CalculateModuleProxyAddressArgs,
+  IsModuleDeployedArgs,
+  IsModuleEnabledArgs
+} from "./types.js"
 
 export class RoleService extends Context.Tag("RoleService")<
   RoleService,
   {
-    buildAssignRolesTx: (args: {
-      module: Address
-      member: Address
-      roleKeys: Array<Hex>
-      memberOf: Array<boolean>
-    }) => Effect.Effect<TransactionData, RoleError>
+    buildAssignRolesTx: (args: BuildAssignRolesTxArgs) => Effect.Effect<TransactionData, RoleError>
 
-    buildDeployModuleTx: (args: {
-      safe: Address
-      saltNonce: bigint
-    }) => Effect.Effect<TransactionData, RoleError>
+    buildDeployModuleTx: (args: BuildDeployModuleTxArgs) => Effect.Effect<TransactionData, RoleError>
 
-    buildScopeFunctionTx: (args: {
-      module: Address
-      roleKey: Hex
-      target: Address
-      selector: Hex
-      conditions: ReadonlyArray<ConditionFlat>
-      executionOpts: ExecutionOptions
-    }) => Effect.Effect<TransactionData, RoleError>
+    buildScopeFunctionTx: (args: BuildScopeFunctionTxArgs) => Effect.Effect<TransactionData, RoleError>
 
-    buildScopeTargetTx: (args: {
-      module: Address
-      roleKey: Hex
-      target: Address
-    }) => Effect.Effect<TransactionData, RoleError>
+    buildScopeTargetTx: (args: BuildScopeTargetTxArgs) => Effect.Effect<TransactionData, RoleError>
 
-    calculateModuleProxyAddress: (args: {
-      safe: Address
-      saltNonce: bigint
-    }) => Effect.Effect<Address, RoleError>
+    calculateModuleProxyAddress: (args: CalculateModuleProxyAddressArgs) => Effect.Effect<Address, RoleError>
 
-    isModuleDeployed: (args: {
-      safe: Address
-      saltNonce: bigint
-    }) => Effect.Effect<boolean, RoleError>
+    isModuleDeployed: (args: IsModuleDeployedArgs) => Effect.Effect<boolean, RoleError>
 
-    isModuleEnabled: (args: {
-      module: Address
-      member: Address
-    }) => Effect.Effect<boolean, RoleError>
+    isModuleEnabled: (args: IsModuleEnabledArgs) => Effect.Effect<boolean, RoleError>
   }
 >() {}
